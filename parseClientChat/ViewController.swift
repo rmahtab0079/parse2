147bb07a7d62354ce68a8b5e6f50d7f00377a6cf
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-03-15 17:36:18 -0400

    Initial Commit

---
+++ parseClientChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parseClientChat
+//
+//  Created by Rageeb Mahtab on 3/15/19.
+//  Copyright © 2019 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
